c8338203c17dc6b9cf4e7f96d882a5916f68fcae
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-22 20:30:30 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,35 @@
 class Customer
   
+  attr_accessor :name, :age
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(waiter, total, tip)
+    food = Meal.new(waiter, self, total, tip)
+    @@all << food
+  end
+  
+  def meals
+    Meal.all.select do |meal| 
+      meal.customer == self
+    end
+  end
+  
+  def waiters
+    meals.map do |meal|
+    meal.waiter
+  end
+  
+  
+end
+  
 end
