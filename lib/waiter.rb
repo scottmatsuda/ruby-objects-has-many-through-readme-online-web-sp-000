e30e5169a30d8b2a498381598c52fc04c66e1912
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-22 20:29:22 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -25,17 +25,10 @@ class Waiter
   end
   
   def best_tipper
-    self.all.each do |meal|
-      meal.tip
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
     end
+    best_tipped_meal.customer
   end
-  
-  # def best_tipper
-  #   best_tipped_meal = meals.max do |meal_a, meal_b|
-  #     meal_a.tip <=> meal_b.tip
-  #   end
- 
-  #   best_tipped_meal.customer
-  # end
   
 end
