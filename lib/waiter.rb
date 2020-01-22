f9b1a8a1f7aee41c3e86cf821aca5a0ddd11ec3b
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-22 20:19:52 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,33 @@
 class Waiter
 
+  attr_accessor :name, :years_of_experience
+  @@all = []
+
+  def initialize(name, years_of_experience)
+    @name = name
+    @years_of_experience = years_of_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def new_meal(customer, total, tip)
+    meal = Meal.new(self, customer, total, tip)
+    @@all << meal
+  end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
+  
+  def best_tipper
+    Meal.all.select do |meal|
+      meal.tip
+    end
+  end
+  
 end
